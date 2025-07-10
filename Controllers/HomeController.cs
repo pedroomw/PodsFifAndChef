25f64b8b8f351371ed37dca94d7c9641c399f939
FIDEL GOLDSMAN <49002222@ort>
2025-07-10 14:24:37 -0300

    cambios 3.0

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,14 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using PodsFifAndChef.Models;
+using Microsoft.Extensions.Logging;
 
 namespace PodsFifAndChef.Controllers;
 
+
+
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+ 
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
 
     public IActionResult Index()
     {
@@ -26,6 +23,12 @@ public class HomeController : Controller
     public IActionResult IniciarSesion(string email, string contraseña)
     {
         Integrante integranteIngresado = BD.BuscarIntegrante(email, contraseña);
+        @ViewBag.Email = integranteIngresado.Email;
+        @ViewBag.Nombre = integranteIngresado.Nombre;
+        @ViewBag.DNI = integranteIngresado.DNI;
+        @ViewBag.Hobby = integranteIngresado.Hobby;
+        @ViewBag.Edad = integranteIngresado.Edad;
+        @ViewBag.Genero = integranteIngresado.Genero;
         if(integranteIngresado != null)
         {
             return View("DatosDelHombre");
